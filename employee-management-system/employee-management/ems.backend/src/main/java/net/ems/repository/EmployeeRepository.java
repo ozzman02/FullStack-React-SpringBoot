e4254bb931af633f0de02fd83acc0247f2264091
osantamaria <osantamaria@gmail.com>
2024-02-22 23:30:21 -0600

    backend - refactoring main folder name

--- employee-management-system/employee-management/ems.backend/src/main/java/net/ems/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package net.ems.repository;
-
-import net.ems.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
