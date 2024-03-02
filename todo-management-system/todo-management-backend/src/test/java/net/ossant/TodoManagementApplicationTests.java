33c9bf0fd1c8f79cdba84df7e2b72037f1e6a19e
osantamaria <osantamaria@gmail.com>
2024-03-02 00:22:32 -0600

    backend - initial project structure

---
+++ todo-management-system/todo-management-backend/src/test/java/net/ossant/TodoManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ossant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
