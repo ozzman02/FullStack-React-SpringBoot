5e28cc1a3c511e987c37805e36999e1c5c794c70
osantamaria <osantamaria@gmail.com>
2024-03-03 01:13:57 -0600

    frontend - rest api calls completed

--- todo-management-system/todo-management-ui/src/App.jsx
+++ todo-management-system/todo-management-ui/src/App.jsx
@@ -3,6 +3,7 @@ import ListTodoComponent from './components/ListTodoComponent'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import TodoComponent from './components/TodoComponent'
 function App() {
   
   return (
@@ -10,8 +11,10 @@ function App() {
       <BrowserRouter>
         <HeaderComponent />
           <Routes>
-            <Route path='/' element={<ListTodoComponent />}></Route>
-            <Route path='/todos' element={<ListTodoComponent />}></Route>
+            <Route path='/' element={ <ListTodoComponent /> }></Route>
+            <Route path='/todos' element={ <ListTodoComponent /> }></Route>
+            <Route path='/add-todo' element={ <TodoComponent /> }></Route>
+            <Route path='/update-todo/:id' element={ <TodoComponent /> }></Route>
           </Routes>  
         <FooterComponent />
       </BrowserRouter> 
--- todo-management-system/todo-management-ui/src/components/ListTodoComponent.jsx
+++ todo-management-system/todo-management-ui/src/components/ListTodoComponent.jsx
@@ -1,15 +1,49 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react"
-import { getAllTodos } from "../services/TodoService"
+import { deleteTodo, getAllTodos, completeTodo, incompleteTodo } from "../services/TodoService"
+import { useNavigate } from "react-router-dom"
 
 const ListTodoComponent = () => {
     
     const [todos, setTodos] = useState([])
 
+    const navigate = useNavigate()
+
+    const addNewTodo = () => navigate('/add-todo')
+
+    const updateTodo = (id) => {
+        navigate(`/update-todo/${id}`)
+    }
+
+    const removeTodo = (id) => {
+        deleteTodo(id).then((response) => {
+            listAllTodos();
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+
     const listAllTodos = () => {
         getAllTodos().then((response) => {
             setTodos(response.data);
         }).catch(error => {
-            console.log(error)
+            console.error(error);
+        })
+    }
+
+    const markTodoAsComplete = (id) => {
+        completeTodo(id).then((response) => {
+            listAllTodos();
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    const markTodoAsInComplete = (id) => {
+        incompleteTodo(id).then((response) => {
+            listAllTodos();
+        }).catch(error => {
+            console.log(error);
         })
     }
     
@@ -20,6 +54,7 @@ const ListTodoComponent = () => {
     return (
         <div className="container">
             <h2 className="text-center">List of Todos</h2>
+            <button className="btn btn-primary mb-2" onClick={addNewTodo}>Add Todo</button>
             <div>
                 <table className="table table-bordered table-striped">
                     <thead>
@@ -27,6 +62,7 @@ const ListTodoComponent = () => {
                             <th>Todo Title</th>
                             <th>Todo Description</th>
                             <th>Todo Completed</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -36,6 +72,31 @@ const ListTodoComponent = () => {
                                     <td>{todo.title}</td>
                                     <td>{todo.description}</td>
                                     <td>{todo.completed ? 'YES' : 'NO'}</td>
+                                    <td>
+                                        <button 
+                                            className="btn btn-info" 
+                                            onClick={() => updateTodo(todo.id)}
+                                        >Update
+                                        </button>
+                                        <button 
+                                            className="btn btn-danger" 
+                                            onClick={() => removeTodo(todo.id)} 
+                                            style={{marginLeft: "10px"}}
+                                        >Delete
+                                        </button>
+                                        <button 
+                                            className="btn btn-success" 
+                                            onClick={() => markTodoAsComplete(todo.id)} 
+                                            style={{marginLeft: "10px"}}
+                                        >Complete
+                                        </button>
+                                        <button 
+                                            className="btn btn-info" 
+                                            onClick={() => markTodoAsInComplete(todo.id)} 
+                                            style={{marginLeft: "10px"}}
+                                        >Incomplete
+                                        </button>
+                                    </td>
                                 </tr>
                             )
                         }
